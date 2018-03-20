7d630001c456f785a6616d1656de308d5adf1c3e
Dmitry Kostarev <albemuth1221@gmail.com>
2018-03-21 01:26:57 +0500

    solution

--- WebServerTemplate/QuestRequestHandler.cs
+++ WebServerTemplate/QuestRequestHandler.cs
@@ -1,6 +1,9 @@
 ﻿using System.Text;
 using System.Net;
 using System.Linq;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace WebServerTemplate
 {
@@ -18,8 +21,8 @@ namespace WebServerTemplate
 			{
 				case "GET":
 					return SelectGetHandler(request);
-				//case "POST":
-				//	return SelectPostHandler(request);
+				case "POST":
+					return SelectPostHandler(request);
 				default:
 					return new NotFoundHandler();
 			}
@@ -32,18 +35,68 @@ namespace WebServerTemplate
 				return new HelloWorldHandler();
 			if(path.Equals("/sum"))
 				return new SumHandler();
+			if(path.Equals("/storage"))
+				return new StorageHandler();
 
 			return new NotFoundHandler();
 		}
 
-		//private IHttpRequestHandler SelectGetHandler(HttpRequestMessage request)
-		//{
-		//	var path = request.Uri.ToString().Split('?')[0];
-		//	if (path.Equals("/sumFromBody"))
-		//	{
-		//		var request = new 
-		//	}
-		//}
+		private IHttpRequestHandler SelectPostHandler(HttpRequestMessage request)
+		{
+			var path = request.Uri.ToString().Split('?')[0];
+			if (path.Equals("/sumFromBody"))
+				return new SumFromBodyHandler();
+			if (path.Equals("/storage"))
+				return new StorageHandler();
+
+			return new NotFoundHandler();
+		}
+	}
+
+	public class StorageHandler : IHttpRequestHandler
+	{
+		private static object storage;
+
+		public HttpResponseMessage Handle(HttpRequestMessage request)
+		{
+			var responseMessage = new HttpResponseMessage
+			{
+				StatusCode = HttpStatusCode.OK,
+			};
+
+			if(request.Method == HttpMethod.Get)
+			{
+				responseMessage.Body = storage as byte[] ?? Encoding.ASCII.GetBytes("Oops");
+			}
+			else
+			{
+				storage = request.Body;
+				responseMessage.Body = Encoding.ASCII.GetBytes("Stored safe and sound");
+			}
+			
+			responseMessage.Headers.Add("Content-length", responseMessage.Body.Length.ToString());
+			responseMessage.Headers.Add("Content-type", "text/html");
+			return responseMessage;
+		}
+	}
+
+	public class SumFromBodyHandler : IHttpRequestHandler
+	{
+		public HttpResponseMessage Handle(HttpRequestMessage request)
+		{
+			var content = JObject.Parse(Encoding.ASCII.GetString(request.Body));
+			var numbers = content["numbers"].ToObject<int[]>();
+
+			var response = JsonConvert.SerializeObject(new {result = numbers.Sum()});
+			var responseMessage = new HttpResponseMessage
+			{
+				StatusCode = HttpStatusCode.OK,
+				Body = Encoding.ASCII.GetBytes(response)
+			};
+			responseMessage.Headers.Add("Content-length", responseMessage.Body.Length.ToString());
+			responseMessage.Headers.Add("Content-type", "text/html");
+			return responseMessage;
+		}
 	}
 
 	public class SumHandler : IHttpRequestHandler
@@ -60,7 +113,7 @@ namespace WebServerTemplate
 				Body = Encoding.ASCII.GetBytes((query["a"]+query["b"]).ToString())
 			};
 			responseMessage.Headers.Add("Content-length", responseMessage.Body.Length.ToString());
-			responseMessage.Headers.Add("Content-type", "text/html");
+			responseMessage.Headers.Add("Content-type", "text/plain");
 			return responseMessage;
 		}
 	}
